057ec3bae0463bf97c15f8d551d98b57b92c1f4e
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-03-07 10:50:43 -0800

    Add bootstrap components and stylings, add background image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import Stack from 'react-bootstrap/Stack';
 
 function App() {
   return (
     <>
+    <Stack className='center-items'>
       <Header />
       <Main />
       <Footer />
+    </Stack>
     </>
   );
 }
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class SearchResult extends React.Component {
     buildStaticMapURL = () => {
-        let mapData = 'https://maps.locationiq.com/v3/staticmap' + 
-        '?key=' + process.env.REACT_APP_LOCATIONIQ_API_KEY + '&center=' +
-        this.props.locData.lat + ',' + this.props.locData.lon + '&zoom=12';
+        let mapData = `https://maps.locationiq.com/v3/staticmap?
+        key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&
+        center=${this.props.locData.lat},${this.props.locData.lon}&zoom=12`;
         return mapData;
     };
 
     render() {
         let results;
-        if (Object.keys(this.props.locData).length > 0) {
+        if (Object.keys(this.props.locData).length > 0 && !this.props.error) {
             results = (
                 <>
-                    <h2>{this.props.locData.display_name}</h2>
-                    <img
-                        src={this.buildStaticMapURL()}
-                        alt={this.props.locData.display_name}
-                    />
-                    <p>Longitude: {this.props.locData.lon}</p>
-                    <p>Latitude: {this.props.locData.lat}</p>
+                    <Card className="text-center card-width">
+                        <Card.Header><h4>{this.props.locData.display_name}</h4></Card.Header>
+                        <Card.Img
+                            variant="top"
+                            className="map-img"
+                            src={this.buildStaticMapURL()}
+                            alt={this.props.locData.display_name}
+                        />
+                        <Card.Body>
+                            <Card.Text>
+                                <strong>Longitude:</strong> {this.props.locData.lon}
+                                <br />
+                                <strong>Latitude:</strong> {this.props.locData.lat}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+
                 </>
             );
         } else {
